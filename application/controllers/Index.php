dc541ed25054e4292a18d15d137e6194bada7dbb
weirubo <weirubo@gmail.com>
2017-12-28 09:24:32 +0800

    yaf-Eloquent

---
+++ application/controllers/Index.php
@@ -0,0 +1,7 @@
+<?php
+class IndexController extends Yaf_Controller_Abstract {
+   //默认Action
+   public function indexAction() {
+       $this->getView()->assign("content", "Hello World");
+   }
+}
