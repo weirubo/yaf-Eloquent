aa3b73173176180185c50c863907783f74384086
weirubo <weirubo@gmail.com>
2018-02-02 11:17:35 +0800

    fix bug

--- application/modules/User/controllers/Profile/v1/Full.php
+++ application/modules/User/controllers/Profile/v1/Full.php
@@ -1,5 +1,9 @@
 <?php
 class Profile_V1_FullController extends BaseController {
+	public function init() {
+		Yaf_Dispatcher::getInstance()->disableView();
+		parent::init();
+	}
 	public function indexAction() {
 		$params = $this->getRequest()->getParams();
 		jsonResult(200, 'success', $params, 'callback');
